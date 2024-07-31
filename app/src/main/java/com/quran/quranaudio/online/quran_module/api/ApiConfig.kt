c6c375d5334754efe99b34b8200503b38eb2ef35
mabaoxiang <mabaoxiang@corp.netease.com>
2024-07-31 22:48:40 +0800

    修改获取音频信息地址

--- app/src/main/java/com/quran/quranaudio/online/quran_module/api/ApiConfig.kt
+++ app/src/main/java/com/quran/quranaudio/online/quran_module/api/ApiConfig.kt
@@ -8,10 +8,18 @@
 package com.quran.quranaudio.online.quran_module.api
 
 object ApiConfig {
-    const val SHAHEEN_DEVELOPERS_URL = "https://cdn.jsdelivr.net/gh/ShaheenDevelopers/apis/"
+  /*  const val SHAHEEN_DEVELOPERS_URL = "https://cdn.jsdelivr.net/gh/ShaheenDevelopers/apis/"
     const val CODECANYON_URL = "https://codecanyon.net/user/shaheendevelopers/portfolio"
     const val WHATSAPP_REPORT_URL = "https://wa.me/+923002375907"
     const val VERSE_REPORT_URL = "https://wa.me/+923002375907"
 
     const val QURAN_API_ROOT_URL = "https://api.quran.com/"
+*/
+    const val SHAHEEN_DEVELOPERS_URL = "https://apis.dochubai.com/quran/"
+    const val CODECANYON_URL = "https://codecanyon.net/user/shaheendevelopers/portfolio"
+    const val WHATSAPP_REPORT_URL = "https://wa.me/+923002375907"
+    const val VERSE_REPORT_URL = "https://wa.me/+923002375907"
+
+    const val QURAN_API_ROOT_URL = "https://api.quran.com/"
+
 }
