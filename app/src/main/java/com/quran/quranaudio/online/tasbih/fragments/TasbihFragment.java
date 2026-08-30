90f12b99de0cb42451cd953923f2a027869cf045
呼伟 <huwei3027@gmail.com>
2026-08-30 19:21:24 +0800

    fix: remove automatic Tasbih rewarded ads

--- app/src/main/java/com/quran/quranaudio/online/tasbih/fragments/TasbihFragment.java
+++ app/src/main/java/com/quran/quranaudio/online/tasbih/fragments/TasbihFragment.java
@@ -42,7 +42,6 @@ public class TasbihFragment extends BaseFragment {
     private int speakStatus;
     private ImageView tasbihView;
     private int total;
-    private int lastRewardedRound = 0; // 已展示激励广告的轮次（每33次/99次算一轮）
     private TextView tv33;
     private TextView tvCount;
     private TextView tvTotal;
@@ -244,11 +243,6 @@ public class TasbihFragment extends BaseFragment {
                     Utils.vibrator();
                 }
                 i = 33;
-                int currentRound = i2 / 33;
-                if (currentRound > lastRewardedRound && getActivity() != null) {
-                    lastRewardedRound = currentRound;
-                    showTasbihRewardedAd();
-                }
             }
         } else {
             int i3 = this.total;
@@ -258,11 +252,6 @@ public class TasbihFragment extends BaseFragment {
                     Utils.vibrator();
                 }
                 i = 99;
-                int currentRound = i3 / 99;
-                if (currentRound > lastRewardedRound && getActivity() != null) {
-                    lastRewardedRound = currentRound;
-                    showTasbihRewardedAd();
-                }
             }
         }
         TextView textView2 = this.tvCount;
@@ -275,16 +264,6 @@ public class TasbihFragment extends BaseFragment {
                 Utils.vibrator();
             }
         }
-    }
-
-    private void showTasbihRewardedAd() {
-        com.quranaudio.common.ad.AdFactory.INSTANCE.loadRewardAd(getActivity(), com.quranaudio.common.ad.AdConfig.AD_TAFSIR_REWARD, null);
-        com.quranaudio.common.ad.AdFactory.INSTANCE.showRewardAd(
-            getActivity(),
-            com.quranaudio.common.ad.AdConfig.AD_TAFSIR_REWARD,
-            "tasbih_round_complete",
-            null
-        );
     }
 
     private void playSound() {
